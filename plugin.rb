dff567cbbc2c829f56fe0b1fdcc61de42bedb821
XtraSweet <127986687+XtraSweet@users.noreply.github.com>
2026-03-25 10:51:18 +0000

    Add XtraSweet as a co-author

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # name: DiscourseJwtSession
 # about: Store the user's session details in a JWT cookie, in addition to the primary session cookie, so it can be accessed from other sites on your domain
 # version: 1.1
-# authors: wilson29thid
+# authors: wilson29thid, XtraSweet
 # url: https://github.com/29th/discourse-jwt-session
 
 enabled_site_setting :jwt_session_enabled
